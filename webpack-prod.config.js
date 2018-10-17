9ddc93c36f76b58652090abbebdf1725899b1d21
Mauro Porras C <mauroporrasc@gmail.com>
2018-10-17 14:20:06 -0500

    WIP-wip

--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -1,24 +1,22 @@
 const path = require('path');
 let fs = require('fs');
 let webpack = require('webpack');
-const UglifyEsPlugin = require('uglify-es-webpack-plugin');
-// const BabiliPlugin = require('babili-webpack-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 let package_json = JSON.parse(fs.readFileSync('package.json'));
 let libraryName = package_json.name;
 
-const plugins = [];
-plugins.push(
-    new UglifyEsPlugin()
-);
 
 module.exports = {
+  mode: 'production',
   entry: './src/index.js',
   output: {
   	filename: `${libraryName}.js`,
     path: path.resolve(__dirname, 'lib'),
     library: libraryName
   },
-  plugins,
-  devtool: 'source-map'
+  devtool: 'source-map',
+  optimization: {
+    minimizer: [new UglifyJsPlugin()]
+  }
 };
