1c48eddf7500078978b1be82530e130f05d31845
phil_visualive <phil@visualive.io>
2018-10-25 12:20:23 -0400

    Remving broken package.

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -6,15 +6,9 @@ let libraryName = package_json.name;
 const plugins = [];
 
 // https://www.npmjs.com/package/copy-webpack-plugin
-const CopyWebpackPlugin = require('copy-webpack-plugin')
 const builddir = path.resolve(__dirname, 'lib');
 const installdir = path.resolve(__dirname, 'projects', 'VisualiveEngine');
 console.log(builddir + `\\${libraryName}.js`)
-plugins.push(
-	new CopyWebpackPlugin([
-      	{from: builddir + `\\${libraryName}.js`, to: installdir },
-      	{from: builddir + `\\${libraryName}.js.map`, to: installdir },
-	], {}));
 
 module.exports = {
   entry: './src/index.js',
