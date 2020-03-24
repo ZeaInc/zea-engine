913f99e33028727cea04e00cd5c8be6593b19a14
Philip Taylor <phil@zea.live>
2020-03-24 14:08:21 -0400

    Migrating configuration to rollup.

--- webpack-dev.config.js
+++ webpack-dev.config.js
@@ -5,13 +5,13 @@ const package_json = JSON.parse(fs.readFileSync('package.json'));
 const { fileName, libraryName } = package_json;
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: `${fileName}.js`,
-        path: path.resolve(__dirname, 'dist'),
-        library: libraryName,
-        libraryTarget: 'umd',
-    },
-    devtool: 'eval-source-map',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    filename: `${fileName}.js`,
+    path: path.resolve(__dirname, 'dist'),
+    library: libraryName,
+    libraryTarget: 'umd',
+  },
+  devtool: 'eval-source-map',
 };
--- webpack-prod.config.js
+++ webpack-prod.config.js
@@ -5,12 +5,12 @@ const package_json = JSON.parse(fs.readFileSync('package.json'));
 const { fileName, libraryName } = package_json;
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.js',
-    output: {
-        filename: `${fileName}.js`,
-        path: path.resolve(__dirname, 'dist'),
-        library: libraryName,
-        libraryTarget: 'umd',
-    }
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    filename: `${fileName}.js`,
+    path: path.resolve(__dirname, 'dist'),
+    library: libraryName,
+    libraryTarget: 'umd',
+  }
 };
