f774cd050b87c6f27179190fe91c7381f75ce03b
Philip Taylor <phil@zea.live>
2020-03-24 19:17:47 -0400

    HelloWorld now imports Classes from ZeaEngine namespace.

--- Testing/Tests/Basic/HelloWorld.js
+++ Testing/Tests/Basic/HelloWorld.js
@@ -1,14 +1,13 @@
-﻿import ZeaEngine from '../../node_modules/@zeainc/zea-engine/dist/zea-engine.esm.js'
+﻿import { Vec3, Scene, GLRenderer } from '../../node_modules/@zeainc/zea-engine/dist/zea-engine.esm.js'
 
-testingHarness.registerTest('Basic/HelloWorld', (domElement, resources) => { 
-    // const Z = ZeaEngine;
+testingHarness.registerTest('Basic/HelloWorld', (domElement, resources) => {
 
-    const scene = new ZeaEngine.Scene(resources);
+    const scene = new Scene(resources);
     scene.setupGrid(5.0, 50);
 
-    const renderer = new ZeaEngine.GLRenderer(domElement);
+    const renderer = new GLRenderer(domElement);
     renderer.setScene(scene);
-    renderer.getViewport().getCamera().setPositionAndTarget(new ZeaEngine.Vec3(2,2,1.7), new ZeaEngine.Vec3(0,0,0.4));
+    renderer.getViewport().getCamera().setPositionAndTarget(new Vec3(2,2,1.7), new Vec3(0,0,0.4));
     renderer.resumeDrawing();
 
     document.addEventListener('keypress', (event) => {
