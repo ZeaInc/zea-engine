a955f7f9b223e9883d2df17bcd9829d91432c842
mauroporras <mauroporrasc@gmail.com>
2020-03-24 18:50:13 -0500

    WIP: feature/migratingToRollup

--- Testing/Tests/Basic/HelloWorld.js
+++ Testing/Tests/Basic/HelloWorld.js
@@ -1,24 +1,27 @@
-﻿import { Vec3, Scene, GLRenderer } from '../../node_modules/@zeainc/zea-engine/dist/zea-engine.esm.js'
+import {
+  Vec3,
+  Scene,
+  GLRenderer,
+} from '../../node_modules/@zeainc/zea-engine/dist/zea-engine.esm.js'
 
 testingHarness.registerTest('Basic/HelloWorld', (domElement, resources) => {
+  const scene = new Scene(resources)
+  scene.setupGrid(5.0, 50)
 
-    const scene = new Scene(resources);
-    scene.setupGrid(5.0, 50);
+  const renderer = new GLRenderer(domElement)
+  renderer.setScene(scene)
+  renderer
+    .getViewport()
+    .getCamera()
+    .setPositionAndTarget(new Vec3(2, 2, 1.7), new Vec3(0, 0, 0.4))
+  renderer.resumeDrawing()
 
-    const renderer = new GLRenderer(domElement);
-    renderer.setScene(scene);
-    renderer.getViewport().getCamera().setPositionAndTarget(new Vec3(2,2,1.7), new Vec3(0,0,0.4));
-    renderer.resumeDrawing();
-
-    document.addEventListener('keypress', (event) => {
-        const key = String.fromCharCode(event.keyCode).toLowerCase();
-        console.log(key)
-        if(key == 'v' && event.shiftKey) {
-            const vrvp = renderer.getVRViewport();
-            if(vrvp) 
-                vrvp.togglePresenting();
-        }
-    });
-
-});
-
+  document.addEventListener('keypress', (event) => {
+    const key = String.fromCharCode(event.keyCode).toLowerCase()
+    console.log(key)
+    if (key == 'v' && event.shiftKey) {
+      const vrvp = renderer.getVRViewport()
+      if (vrvp) vrvp.togglePresenting()
+    }
+  })
+})
