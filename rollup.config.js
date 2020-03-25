68168470c98b329b3b00f5d752c5e64a0ca458eb
mauroporras <mauroporrasc@gmail.com>
2020-03-24 20:02:31 -0500

    WIP: feature/migratingToRollup

--- rollup.config.js
+++ rollup.config.js
@@ -1,25 +1,9 @@
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
 import bundleWorker from 'rollup-plugin-bundle-worker'
 import { terser } from 'rollup-plugin-terser'
 
 import pkg from './package.json'
 
 export default [
-  // browser-friendly UMD build
-  {
-    input: 'src/index.js',
-    output: {
-      name: 'ZeaEngine',
-      file: pkg.browser,
-      format: 'umd',
-    },
-    plugins: [
-      resolve(), // so Rollup can find `dependencies`
-      commonjs(), // so Rollup can convert `dependencies` to ES modules
-      bundleWorker(),
-    ],
-  },
   // CommonJS (for Node) and ES module (for bundlers) build.
   // (We could have three entries in the configuration array
   // instead of two, but it's quicker to generate multiple
