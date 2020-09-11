eb28d1176a571310640c3eb53c8140242438b298
mauroporras <mauroporrasc@gmail.com>
2020-09-11 09:56:31 -0500

    WIP: umd-support

--- cypress/integration/zea-engine-perf-spec.js
+++ cypress/integration/zea-engine-perf-spec.js
@@ -1,6 +1,6 @@
 describe('Zea Engine-Performance', () => {
   it('stress test memory', () => {
-    cy.visit('memory-test.html', { timeout: 70000 })
+    cy.visit('testing-e2e/memory-test.html', { timeout: 70000 })
     cy.get('canvas').percySnapshot(`memory-test`)
   })
 })
