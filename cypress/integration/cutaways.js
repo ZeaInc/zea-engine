0f9a790acc233088ec25eca6f4f3dee68090d49f
mauroporras <mauroporrasc@gmail.com>
2020-10-06 08:16:33 -0500

    WIP: e2e-test-cutaways

--- cypress/integration/cutaways.js
+++ cypress/integration/cutaways.js
@@ -1,5 +1,5 @@
 describe('Zea Engine', () => {
-  it('Renders cutawais', () => {
+  it('Renders cutaways', () => {
     cy.visit('testing-e2e/cutaways.html')
     cy.get('canvas').percySnapshot('Cutaways')
   })
