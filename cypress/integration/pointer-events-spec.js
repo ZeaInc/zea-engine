03c246c7f407c95bdc68e19971c01c3d632746e3
Alvaro <alvaroe.pajaro@gmail.com>
2020-10-14 09:52:17 -0500

    fix: Pointer events now wait the right time for snapshots

--- cypress/integration/pointer-events-spec.js
+++ cypress/integration/pointer-events-spec.js
@@ -9,115 +9,114 @@ describe('pointer-events', () => {
     })
   })
 
-  describe('Mouse Events', () => {
-    it('Move - Camera Manipulator', () => {
-      cy.get('canvas')
-        .trigger('mousedown', 600, 500)
-        .trigger('mousemove', 650, 500)
-        .trigger('mouseup', 650, 500)
-        .percySnapshot(`MoveCameraManipulator`)
-    })
+  it('Mouse Move - Camera Manipulator', () => {
+    cy.get('canvas').trigger('mousedown', 600, 500).trigger('mousemove', 650, 500).trigger('mouseup', 650, 500)
 
-    it('Mouse Enter - Geometry', () => {
-      cyFocusCanvas()
-
-      cy.get('canvas').trigger('mousemove', 100, 230).trigger('mousemove', 250, 230).percySnapshot(`MouseEnterGeometry`)
-    })
-
-    it('Mouse Leave - Geometry', () => {
-      cyFocusCanvas()
-
-      cy.get('canvas').trigger('mousemove', 250, 230).trigger('mousemove', 100, 230).percySnapshot(`MouseLeaveGeometry`)
-    })
-
-    it('Zoom In - Camera Manipulator', () => {
-      cyFocusCanvas()
-
-      cy.get('canvas').trigger('wheel', {
-        deltaX: -0,
-        deltaY: -200,
-        deltaZ: 0,
-      })
-
-      cy.get('@postMessage').its('lastCall.args.0').should('equal', 'done-wheeling')
-      cy.percySnapshot(`ZoomInCameraManipulator`)
-    })
-
-    it('Zoom Out - Camera Manipulator', () => {
-      cyFocusCanvas()
-
-      cy.get('canvas').trigger('wheel', {
-        deltaX: -0,
-        deltaY: 500,
-        deltaZ: 0,
-      })
-
-      cy.get('@postMessage')
-        .its('lastCall.args.0')
-        .should('equal', 'done-wheeling')
-        .percySnapshot(`ZoomOutCameraManipulator`)
-    })
-
-    it('Double Click - Geometry', () => {
-      cy.get('canvas')
-        .trigger('mousedown', 800, 300)
-        .trigger('mouseup', 800, 300)
-        .trigger('mousedown', 800, 300)
-        .trigger('mouseup', 800, 300)
-
-      cy.get('@postMessage')
-        .its('lastCall.args.0')
-        .should('equal', 'done-double-down-on-geom')
-        .percySnapshot(`DoubleClickGeometry`)
-    })
+    cy.get('@postMessage')
+      .its('lastCall.args.0')
+      .should('equal', 'done-moving-camera')
+      .percySnapshot(`MouseMoveCameraManipulator`)
   })
 
-  describe('Touch Events', () => {
-    it('Move - Camera manipulator', () => {
-      const eTouchStart = createTouchEvents([600, 600])
-      const eTouch = createTouchEvents([550, 600])
+  it('Mouse Enter - Geometry', () => {
+    cyFocusCanvas()
 
-      cy.get('canvas')
-        .trigger('touchstart', eTouchStart)
-        .trigger('touchmove', eTouch)
-        .trigger('touchend', eTouch)
-        .percySnapshot(`DoubleClickGeometry`)
+    cy.get('canvas').trigger('mousemove', 100, 230).trigger('mousemove', 250, 230).percySnapshot(`MouseEnterGeometry`)
+  })
+
+  it('Mouse Leave - Geometry', () => {
+    cyFocusCanvas()
+
+    cy.get('canvas').trigger('mousemove', 250, 230).trigger('mousemove', 100, 230).percySnapshot(`MouseLeaveGeometry`)
+  })
+
+  it('Wheel Zoom In - Camera Manipulator', () => {
+    cyFocusCanvas()
+
+    cy.get('canvas').trigger('wheel', {
+      deltaX: -0,
+      deltaY: -200,
+      deltaZ: 0,
     })
 
-    it('Double Tap - Geometry', () => {
-      const eTouch = createTouchEvents([800, 300])
-      cy.get('canvas')
-        .trigger('touchstart', eTouch)
-        .trigger('touchend', eTouch)
-        .trigger('touchstart', eTouch)
-        .trigger('touchend', eTouch)
+    cy.get('@postMessage')
+      .its('lastCall.args.0')
+      .should('equal', 'done-moving-camera')
+      .percySnapshot(`WheelZoomInCameraManipulator`)
+  })
 
-      cy.get('@postMessage')
-        .its('lastCall.args.0')
-        .should('equal', 'done-double-down-on-geom')
-        .percySnapshot(`DoubleClickGeometry`)
+  it('Wheel Zoom Out - Camera Manipulator', () => {
+    cyFocusCanvas()
+
+    cy.get('canvas').trigger('wheel', {
+      deltaX: -0,
+      deltaY: 500,
+      deltaZ: 0,
     })
 
-    it('Zoom In - Camera manipulator', () => {
-      const eTouchStart = createTouchEvents([600, 600, 650, 600])
-      const eTouch = createTouchEvents([500, 600, 700, 600])
+    cy.get('@postMessage')
+      .its('lastCall.args.0')
+      .should('equal', 'done-moving-camera')
+      .percySnapshot(`WheelZoomOutCameraManipulator`)
+  })
 
-      cy.get('canvas')
-        .trigger('touchstart', eTouchStart)
-        .trigger('touchmove', eTouch)
-        .trigger('touchend', eTouch)
-        .percySnapshot(`DoubleClickGeometry`)
-    })
+  it('Double Click - Geometry', () => {
+    cy.get('canvas')
+      .trigger('mousedown', 800, 300)
+      .trigger('mouseup', 800, 300)
+      .trigger('mousedown', 800, 300)
+      .trigger('mouseup', 800, 300)
 
-    it('Zoom Out - Camera manipulator', () => {
-      const eTouchStart = createTouchEvents([500, 600, 700, 600])
-      const eTouch = createTouchEvents([600, 600, 650, 600])
+    cy.get('@postMessage')
+      .its('lastCall.args.0')
+      .should('equal', 'done-moving-camera')
+      .percySnapshot(`DoubleClickGeometry`)
+  })
 
-      cy.get('canvas')
-        .trigger('touchstart', eTouchStart)
-        .trigger('touchmove', eTouch)
-        .trigger('touchend', eTouch)
-        .percySnapshot(`DoubleClickGeometry`)
-    })
+  it('Touch Move - Camera manipulator', () => {
+    const eTouchStart = createTouchEvents([600, 600])
+    const eTouch = createTouchEvents([550, 600])
+
+    cy.get('canvas')
+      .trigger('touchstart', eTouchStart)
+      .trigger('touchmove', eTouch)
+      .trigger('touchend', eTouch)
+      .percySnapshot(`TouchMoveCameraManipulator`)
+  })
+
+  it('Double Tap - Geometry', () => {
+    const eTouch = createTouchEvents([800, 300])
+    cy.get('canvas')
+      .trigger('touchstart', eTouch)
+      .trigger('touchend', eTouch)
+      .trigger('touchstart', eTouch)
+      .trigger('touchend', eTouch)
+
+    cy.get('@postMessage')
+      .its('lastCall.args.0')
+      .should('equal', 'done-moving-camera')
+      .percySnapshot(`DoubleTapGeometry`)
+  })
+
+  it('Touch Zoom In - Camera manipulator', () => {
+    const eTouchStart = createTouchEvents([600, 600, 650, 600])
+    const eTouch = createTouchEvents([500, 600, 700, 600])
+
+    cy.get('canvas')
+      .trigger('touchstart', eTouchStart)
+      .trigger('touchmove', eTouch)
+      .trigger('touchend', eTouch)
+      .percySnapshot(`TouchZoomInCameraManipulator`)
+  })
+
+  it('Touch Zoom Out - Camera manipulator', () => {
+    const eTouchStart = createTouchEvents([500, 600, 700, 600])
+    const eTouch = createTouchEvents([600, 600, 650, 600])
+
+    cy.get('canvas')
+      .trigger('touchstart', eTouchStart)
+      .trigger('touchmove', eTouch)
+      .trigger('touchend', eTouch)
+      .percySnapshot(`TouchZoomOutCameraManipulator`)
   })
 })
