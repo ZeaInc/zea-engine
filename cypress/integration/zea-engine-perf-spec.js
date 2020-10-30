687842a83ebb1bf804f5ed2c22140fbaa270fa6f
mauroporras <mauroporrasc@gmail.com>
2020-10-30 08:45:00 -0500

    WIP: poc-e2e

--- cypress/integration/zea-engine-perf-spec.js
+++ cypress/integration/zea-engine-perf-spec.js
@@ -1,6 +1,7 @@
 describe('Zea Engine performance', () => {
   it('Behaves properly under memory stress', () => {
-    cy.visit('testing-e2e/memory-test.html', { timeout: 70000 })
+    cy.visit('testing-e2e/memory-test.html')
+    cy.get('#status', { timeout: 70000 }).should('have.text', 'Done')
     cy.get('canvas').percySnapshot('Memory test')
   })
 })
