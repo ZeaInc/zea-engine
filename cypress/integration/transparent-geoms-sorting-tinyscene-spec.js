b122482faf1a6c251d2d31b09201cb06fa420c24
Philip Taylor <phil@zea.live>
2020-11-10 11:13:06 -0500

    test: fixed spec for tiny scene sorting.

--- cypress/integration/transparent-geoms-sorting-tinyscene-spec.js
+++ cypress/integration/transparent-geoms-sorting-tinyscene-spec.js
@@ -1,11 +1,11 @@
-describe('transparent-geoms-sorting-tinyscene-spec', () => {
+describe('transparent-geoms-sorting-tinyscene', () => {
   it('Captures a snapshot', () => {
-    cy.visit('testing-e2e/transparent-geoms-sorting-tinyscene-spec.html')
-    cy.get('canvas').percySnapshot('transparent-geoms-sorting-tinyscene-spec')
+    cy.visit('testing-e2e/transparent-geoms-sorting-tinyscene.html')
+    cy.get('canvas').percySnapshot('transparent-geoms-sorting-tinyscene')
   })
 
   it.skip('Captures snapshots of variants', () => {
-    cy.visit('testing-e2e/transparent-geoms-sorting-tinyscene-spec.html', {
+    cy.visit('testing-e2e/transparent-geoms-sorting-tinyscene.html', {
       onBeforeLoad(win) {
         cy.spy(win, 'postMessage').as('postMessage')
       },
@@ -17,14 +17,14 @@ describe('transparent-geoms-sorting-tinyscene-spec', () => {
       const variant = 'variant-01'
       win.postMessage(variant)
       cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene-spec - ${variant}`)
+      cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene - ${variant}`)
     })
 
     cy.window().then((win) => {
       const variant = 'variant-02'
       win.postMessage(variant)
       cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-${variant}`)
-      cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene-spec - ${variant}`)
+      cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene - ${variant}`)
     })
   })
 })
