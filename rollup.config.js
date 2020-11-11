541e6822c4c2a3aae42597ddd164665c7ce27b3d
mauroporras <mauroporrasc@gmail.com>
2020-11-11 15:55:24 -0500

    WIP: resource-loader-plugins

--- rollup.config.js
+++ rollup.config.js
@@ -41,6 +41,21 @@ export default [
     plugins,
   },
 
+  // CommonJS (for Node) and ES module (for bundlers) build.
+  // (We could have three entries in the configuration array
+  // instead of two, but it's quicker to generate multiple
+  // builds from a single configuration where possible, using
+  // an array for the `output` option, where we can specify
+  // `file` and `format` for each target)
+  {
+    input: 'src/index.js',
+    output: [
+      { file: pkg.main, format: 'cjs', sourcemap },
+      { file: pkg.module, format: 'es', sourcemap },
+    ],
+    plugins,
+  },
+
   // Zea Engine default plugins.
   {
     input: 'src/index-plugins.js',
