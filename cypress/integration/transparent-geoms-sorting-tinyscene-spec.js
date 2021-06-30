8b93f7d0b45ede633e2d294bfecbc703fa2dc530
Philip Taylor <phil@zea.live>
2021-06-30 14:52:57 -0400

    test: fixed tiny scene as a race condition meant sometimes culling was not updated before the snapshot was taken.

--- cypress/integration/transparent-geoms-sorting-tinyscene-spec.js
+++ cypress/integration/transparent-geoms-sorting-tinyscene-spec.js
@@ -3,13 +3,11 @@ describe('transparent-geoms-sorting-tinyscene', () => {
     cy.visit('testing-e2e/transparent-geoms-sorting-tinyscene.html')
 
     cy.get('#front').click()
-
-    cy.get('canvas').percySnapshot(
-      `transparent-geoms-sorting-tinyscene - front`
-    )
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-front`)
+    cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene - front`)
 
     cy.get('#back').click()
-
+    cy.get('@postMessage').its('lastCall.args.0').should('equal', `done-back`)
     cy.get('canvas').percySnapshot(`transparent-geoms-sorting-tinyscene - back`)
   })
 })
