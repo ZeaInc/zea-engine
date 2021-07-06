98901e5bc0d584d86d9971b3dc6421f117960475
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-06 11:07:51 -0700

    Normals Shader - removed inline impl

--- src/Renderer/Shaders/NormalsShader.js
+++ src/Renderer/Shaders/NormalsShader.js
@@ -4,6 +4,9 @@ import { GLShader } from '../GLShader.js'
 import './GLSL/stack-gl/inverse.js'
 import './GLSL/stack-gl/transpose.js'
 
+import frag from './GLSLFiles/Normals.frag.glsl'
+import vert from './GLSLFiles/Normals.vert.glsl'
+
 class NormalsShader extends GLShader {
   /**
    * Create a GL shader.
@@ -11,60 +14,9 @@ class NormalsShader extends GLShader {
    */
   constructor(gl) {
     super(gl)
-    this.setShaderStage(
-      'VERTEX_SHADER',
-      `
-precision highp float;
-
-instancedattribute vec3 positions;
-instancedattribute vec3 normals;
-attribute vec2 vertexIDs;
-
-uniform mat4 viewMatrix;
-uniform mat4 projectionMatrix;
-
-uniform float normalLength;
-
-<%include file="stack-gl/transpose.glsl"/>
-<%include file="drawItemTexture.glsl"/>
-<%include file="modelMatrix.glsl"/>
-
-
-/* VS Outputs */
-varying float v_weight;
-
-void main(void) {
-  mat4 modelMatrix = getModelMatrix(drawItemId);
-  mat4 modelViewProjectionMatrix = projectionMatrix * viewMatrix * modelMatrix;
-  if(vertexIDs.x == 0.0){
-    gl_Position = modelViewProjectionMatrix * vec4(positions, 1.0);
-    v_weight = 1.0;
-  }
-  else{
-    gl_Position = modelViewProjectionMatrix * vec4(positions+(normals*normalLength), 1.0);
-    v_weight = 0.0;
-  }
-}
-`
-    )
-
-    this.setShaderStage(
-      'FRAGMENT_SHADER',
-      `
-precision highp float;
-
-uniform color normalColor;
-
-/* VS Outputs */
-varying float v_weight;
-
-
-void main(void) {
-  gl_FragColor = normalColor;
-  gl_FragColor.a = v_weight;
-}
-`
-    )
+    this.setShaderStage('VERTEX_SHADER', vert)
+    this.setShaderStage('FRAGMENT_SHADER', frag)
+    // this.finalize() TODO: should a finalize() be here?
   }
 }
 
