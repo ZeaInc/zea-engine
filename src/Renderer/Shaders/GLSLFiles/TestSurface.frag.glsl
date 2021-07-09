eca9e6de2d3f0d8b6982dce4dd76f4326d437c87
Seiji-zea Smith <seiji.smith@zea.live>
2021-07-09 11:19:18 -0700

    simplified vertex shader
    - for easier debugging

--- src/Renderer/Shaders/GLSLFiles/TestSurface.frag.glsl
+++ src/Renderer/Shaders/GLSLFiles/TestSurface.frag.glsl
@@ -1,14 +1,15 @@
 
 precision highp float;
-
-import 'GLSLUtils.glsl'
-import 'stack-gl/gamma.glsl'
-import 'materialparams.glsl'
 out vec4 fragColor;
+
+// import 'GLSLUtils.glsl'
+// import 'stack-gl/gamma.glsl'
+// import 'materialparams.glsl'
+
 
 // import 'testSnippet/snippet.glsl'
 // import 'testSnippet/snippet2.glsl'
 
 void main(void) {
-  fragColor = vec4(0.1,0.1,1.0,1.0);
+  fragColor = vec4(0.1,0.1,0.6,1.0);
 }
